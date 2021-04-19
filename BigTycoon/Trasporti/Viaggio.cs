3b797600daf6631f38d7d00125bbf1357636a604
EatPastaKillFasta <78098716+EatPastaKillFasta@users.noreply.github.com>
2021-04-19 10:29:50 +0200

    metodo update/updateAll

--- BigTycoon/Trasporti/Viaggio.cs
+++ BigTycoon/Trasporti/Viaggio.cs
@@ -15,17 +15,22 @@ namespace BigTycoon.Trasporti
 		public float DurataTotale { get; set; }
 		public float DurataCorrente { get; set; }
 		public bool Visible { get; set; }
-		public int Indice { get; set; }
+		public int Indice { get; set; }					//indice nel vettore di viaggi attivi
 
-		public Viaggio(Edificio mittente, Edificio destinatario, Oggetto[] merce, int nVagoni, int indice)
+		private float unitaPercentuale;					//quantita per cui viene incrementata la percentuale di completamento
+
+		public Viaggio(Edificio mittente, Edificio destinatario, Oggetto[] merce, int nVagoni, int indice, int durata)
 		{
 			Vagoni = new Oggetto[nVagoni];
 			Destinatario = destinatario;
 			Mittente = mittente;
 			Indice = indice;
+			DurataTotale = durata;
 
+			unitaPercentuale = DurataTotale / 100;
 			CapacitaVagone = 10;
 			CostoVagone = 50;
+			DurataCorrente = 0;
 		}
 
 		public void DisegnaLinea()
@@ -38,9 +43,9 @@ namespace BigTycoon.Trasporti
 
         }
 
-		public void Update()
-        {
-
-        }
+		public void Update()									//aggiorna la percentuale di completamento dei ogni viaggio
+		{
+			DurataCorrente += unitaPercentuale;
+		}
 	}
 }
